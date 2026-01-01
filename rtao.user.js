f651ca6851ad34aa618069313d342cbc80240805
RTaO Dev <warutchamchoy789@gmail.com>
2026-01-01 21:39:53 +0700

    Create rtao.user.js

---
+++ rtao.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         RTaO Bypass
+// @namespace    rtaobypasser
+// @version      0.0.2
+// @description  RTaO Bypass
+// @author       rtao.exe
+// @match        https://work.ink/*
+// @match        *://direct-link.net/*
+// @match        *://link-target.net/*
+// @match        *://link-to.net/*
+// @match        *://link-center.net/*
+// @match        *://link-hub.net/*
+// @match        *://up-to-down.net/*
+// @match        *://linkvertise.com/*
+// @match        *://auth.platorelay.com/*
+// @match        *://auth.platoboost.me/*
+// @match        *://auth.platoboost.app/*
+// @match        *://pandadevelopment.net/*
+// @match        *://keyrblx.com/*
+// @match        *://bstlar.com/*
+// @match        *://rekonise.com/*
+// @match        *://www.rekonise.com/*
+// @match        *://mboost.me/*
+// @match        *://ads.luarmor.net/*
+// @match        *://loot-link.com/s?*
+// @match        *://loot-links.com/s?*
+// @match        *://lootlink.org/s?*
+// @match        *://lootlinks.co/s?*
+// @match        *://lootdest.info/s?*
+// @match        *://lootdest.org/s?*
+// @match        *://lootdest.com/s?*
+// @match        *://links-loot.com/s?*
+// @match        *://linksloot.net/s?*
+// @run-at       document-end
+// @downloadURL  https://github.com/elfuhh/bypass/raw/main/bypass.user.js
+// @updateURL    https://github.com/elfuhh/bypass/raw/main/bypass.user.js
+// @require https://github.com/elfuhh/bypass/raw/refs/heads/main/bypass.js
+// ==/UserScript==
